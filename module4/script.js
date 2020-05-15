fc16da445092217e21e660813b0f0a14f0273a60
MichaelJetson <64679440+MichaelJetson@users.noreply.github.com>
2020-05-15 19:38:40 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,7 +1,5 @@
-
-
 (function() {
-    var names = ["Jetson", "Michael", " Jhon", "Ram", "Peter", "Lewis", "Jam", "Brick", "Pasta", "Jack"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
